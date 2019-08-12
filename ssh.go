e29a0f03172f3b435f7d259771494b7701309597
胡杨 <chuang.xie@enai.im>
2019-08-12 18:27:58 +0800

    add gocmd config

--- ssh.go
+++ ssh.go
@@ -16,10 +16,35 @@ type GocmdConfig struct {
 	Ip string
 	// 连接端口号
 	Port string
+	// 连接用户名
+	Username string
 	// 连接密码
 	Password string
 	// 认证类型 [1: password 2: ssh key]
 	AuthType int
 	// ~/.ssh/id_rsa
-	keyPath string
+	KeyPath string
+}
+
+// ReloadGocmdConfig 初始化ssh config
+func Config(ip, port, username, password, keyPath string, authType ...int) *GocmdConfig {
+	config := &GocmdConfig{
+		Ip:       ip,
+		Username: username,
+		Password: password,
+		KeyPath:  keyPath,
+	}
+	// default 22
+	if len(port) == 0 {
+		config.Port = DefaultSshPort
+	} else {
+		config.Port = port
+	}
+	// default password
+	if len(authType) == 0 {
+		config.AuthType = DefaultAuthType
+	} else {
+		config.AuthType = authType[0]
+	}
+	return config
 }
